d35e27ad7a9583eb8728773217d2f4cdd6bbc616
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-16 23:44:32 -0400

    Create route_53.tf

---
+++ route_53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" {
+  zone_id = "Z06528912840OFLNFP3WA"
+  name    = "jenkins.hyavuz.com"
+  type    = "A"
+  ttl     = "5"
+  records = ["${aws_instance.web.public_ip}"]
+}
