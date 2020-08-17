a070ac1e76bb90e9a4d262fa0f388fa8271ec853
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-17 00:01:01 -0400

    Update route_53.tf

--- route_53.tf
+++ route_53.tf
@@ -2,6 +2,7 @@ resource "aws_route53_record" "www" {
   zone_id = "Z06528912840OFLNFP3WA"
   name    = "jenkins.hyavuz.com"
   type    = "A"
-  ttl     = "5"
+  ttl     = "30"
   records = ["${aws_instance.web.public_ip}"]
 }
+
